442c11a920a200a5581dc80895a743b24ecd09c8
AbishekPerera <abhishekperera77@gmail.com>
2023-03-19 11:24:12 +0530

    bill management update code cleaning

--- BillManagePublisher/src/billmanagepublisher/BillManageService.java
+++ BillManagePublisher/src/billmanagepublisher/BillManageService.java
@@ -179,6 +179,7 @@ public class BillManageService implements BillManageServiceInterface {
 		// TODO Auto-generated method stub
 
 		int billId;
+		String sql="";
 		BillModel bill = new BillModel();
 
 		System.out.println(":::::::::::::::::::::::::::::::::::::::::::::::::::::");
@@ -209,37 +210,67 @@ public class BillManageService implements BillManageServiceInterface {
 						bill.getCusName(), bill.getPhoneModel(), bill.getPhoneBrand(), bill.getPhoneImei(),
 						bill.getPrice(), bill.getDiscount(), bill.getTotal());
 
-				System.out.println("Enter New Customer Name: ");
-				scan.nextLine();
-				bill.setCusName(scan.nextLine());
+				System.out.println("..........................\n");
+				System.out.println("wich field you want to update ?\n");
+				System.out.println("1. Customer Name");
+				System.out.println("2. Phone Model");
+				System.out.println("3. Phone Brand");
+				System.out.println("4. Phone IMEI");
+				System.out.println("5. Phone Price & Discount");
+				System.out.println("\n.......................\n");
 
-				System.out.println("Enter new Phone Model : ");
-				bill.setPhoneModel(scan.next().trim());
+				System.out.print("Enter your choice : ");
+				int choice = Integer.parseInt(scan.next().trim());
 
-				System.out.println("Enter new Phone Brand : ");
-				bill.setPhoneBrand(scan.next().trim());
+				switch (choice) {
+				case 1:
+					System.out.println("Enter New Customer Name: ");
+					scan.nextLine();
+					bill.setCusName(scan.nextLine());
+					sql = "UPDATE bills SET CusName='" + bill.getCusName() + "' WHERE id = " + billId;
+					break;
+				case 2:
+					System.out.println("Enter new Phone Model : ");
+					bill.setPhoneModel(scan.next().trim());
+					sql = "UPDATE bills SET PhoneModel='" + bill.getPhoneModel() + "' WHERE id = " + billId;
+					break;
+				case 3:
+					System.out.println("Enter new Phone Brand : ");
+					bill.setPhoneBrand(scan.next().trim());
+					sql = "UPDATE bills SET PhoneBrand='" + bill.getPhoneBrand() + "' WHERE id = " + billId;
+					break;
+				case 4:
+					System.out.println("Enter new Phone IMEI : ");
+					bill.setPhoneImei(scan.next().trim());
+					sql = "UPDATE bills SET PhoneImei='" + bill.getPhoneImei() + "' WHERE id = " + billId;
+					break;
+				case 5:
+					System.out.println("Enter new Phone Price : ");
+					bill.setPrice(Double.parseDouble(scan.next().trim()));
+					
+					System.out.println("Enter new Discount : ");
+					bill.setDiscount(Double.parseDouble(scan.next().trim()));
 
-				System.out.println("Enter new Phone IMEI : ");
-				bill.setPhoneImei(scan.next().trim());
+					double total = bill.getPrice() - bill.getDiscount();
 
-				System.out.println("Enter new Phone Price : ");
-				bill.setPrice(Double.parseDouble(scan.next().trim()));
+					sql = "UPDATE bills SET price=" + bill.getPrice() + ", discount=" + bill.getDiscount() + ", total="
+							+ total + " WHERE id = " + billId;
+					break;
 
-				System.out.println("Enter new Discount : ");
-				bill.setDiscount(Double.parseDouble(scan.next().trim()));
+				default:
+					System.out.println("Invalid choice");
 
-				double total = bill.getPrice() - bill.getDiscount();
-				bill.setTotal(total);
+				}
 
-				System.out.println("Enter new Total : " + bill.getTotal());
+				try{
+					statement = connection.createStatement();
+					statement.executeUpdate(sql);
+					System.out.println("Bill updated successfully");
 
-				String sql = "UPDATE bills SET CusName='" + bill.getCusName() + "', PhoneModel='" + bill.getPhoneModel()
-						+ "', PhoneBrand='" + bill.getPhoneBrand() + "', PhoneImei='" + bill.getPhoneImei()
-						+ "', price=" + bill.getPrice() + ", discount=" + bill.getDiscount() + ", total="
-						+ bill.getTotal() + " WHERE id=" + billId;
-
-				statement.executeUpdate(sql);
-				System.out.println("Bill updated successfully");
+				}catch (SQLException exc) {
+					System.out.println("Error updating bill from database");
+					System.out.println(exc.getMessage());
+				}
 
 			} else {
 				System.out.println("No bill found with the given ID");
